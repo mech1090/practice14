344e37228c71ad9079e6e59915c148f7366919e1
Shahzad <mech1090@gmail.com>
2022-06-20 11:45:22 -0600

    routes and controller  created

---
+++ controller/user.controller.js
@@ -0,0 +1,9 @@
+
+
+
+const getLoginFrom = (req,res)=>{}
+const login = (req,res)=>{}
+const getSignupForm = (req,res)=>{}
+const signup = (req,res)=>{}
+
+module.exports = {getLoginFrom,login,getSignupForm,signup}
